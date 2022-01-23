6c6cb9c70da203f9cf5c8eb1575885d93553e11a
Eternal-Faith <2295616516@qq.com>
2022-01-23 23:10:31 +0800

    完善接口

--- model/model.go
+++ model/model.go
@@ -1,6 +1,8 @@
 package model
 
 //gorm:"column:id",是为为对应的sql语句中的字段起名字。gorm自动生成sql时大写字母转小写还会生成下划线
+//id应该都设为int不然输不进去数据
+//前端传数据时一定要传有json的tag的数据
 type User struct {
 	UserId   int    `json:"user_id" gorm:"column:id"`
 	Phone    string `json:"phone"`
@@ -11,49 +13,49 @@ type User struct {
 }
 
 type Team struct {
-	TeamId     string `json:"team_id" gorm:"column:id`
-	TeamName   string `json:"teamname" gorm:"column:name`
+	TeamId     int    `json:"team_id" gorm:"column:id"`
+	TeamName   string `json:"teamname" gorm:"column:name"`
 	Avatar     string `json:"avatar"`
 	Creator    string `json:"creator"`
 	TeamCoding string `json:"team_coding"`
 }
 
 type UserTeam struct {
-	Id       string `json:"id"`
-	UserName string `json:"username"`
-	TeamName string `json:"teamname"`
+	Id       int    `json:"id"`
+	UserName string `json:"username" gorm:"column:username"`
+	TeamName string `json:"teamname" gorm:"column:teamname"`
 }
 
 type Project struct {
-	ProjectId     string `json:"project_id"`
-	ProjectName   string `json:"project_name"`
-	Creator       string `json:"creator"`
-	CreateTime    string `json:"createtime"`
-	StartTime     string `json:"starttime"`
-	Deadline      string `json:"deadline"`
-	ProjectRemark string `json:"projectremark"`
-	TeamName      string `json:"teamname"`
+	ProjectId   int    `json:"project_id" gorm:"column:id"`
+	ProjectName string `json:"project_name" gorm:"column:name"`
+	Creator     string `json:"creator"`
+	CreateTime  string `json:"create_time"`
+	StartTime   string `json:"start_time"`
+	Deadline    string `json:"deadline"`
+	Remark      string `json:"remark"`
+	TeamName    string `json:"teamname" gorm:"column:team"`
 }
 
 type Step struct {
-	StepId     string `json:"step_id"`
-	StepName   string `json:"step_name"`
-	CreateTime string `json:"createtime"`
-	StartTime  string `json:"starttime"`
-	Deadline   string `json:"deadline"`
-	StepRemark string `json:"stepremark"`
-	TeamName   string `json:"teamname"`
+	StepId      int    `json:"step_id" gorm:"column:id"`
+	StepName    string `json:"step_name" gorm:"column:name"`
+	CreateTime  string `json:"createtime" gorm:"column:create_team"`
+	StartTime   string `json:"start_time" gorm:"column:start_team"`
+	Deadline    string `json:"deadline"`
+	Remark      string `json:"remark"`
+	ProjectName string `json:"project_name" gorm:"column:project"`
 }
 
 type Task struct {
-	TaskId  string `json:"task_id"`
-	Content string `json:"content"`
+	TaskId  int    `json:"task_id" gorm:"column:id"`
+	Content string `json:"content" `
 	Step    string `json:"step"`
 }
 
 type UserTask struct {
-	Id          string `json:"id"`
-	UserName    string `json:"username"`
-	TaskName    string `json:"taskname"`
+	Id          int    `json:"id"`
+	UserName    string `json:"username" gorm:"column:principal"`
+	TaskName    string `json:"taskname" gorm:"column:task_id"`
 	Performance bool   `json:"performance"`
 }
--- router/router.go
+++ router/router.go
@@ -6,10 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//POST新建，put更新
 func Router(r *gin.Engine) {
 	r.POST("/user", handler.User)
+	r.POST("/user/pupup", handler.InitUserInfo)
 	r.POST("/login", handler.Login)
 	r.GET("/info", handler.Userinfo)
 	r.PUT("/info", handler.ChangeInfomation)
-	r.PUT("/create_team", handler.CreateTeam)
+	r.POST("/create_team", handler.CreateTeam)
+	r.POST("/join_team", handler.JoinTeam)
+	r.POST("/create_project/:team", handler.CreateProject)
 }
